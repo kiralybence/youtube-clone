91eea6dd3d86ef780b78b68e2e9e256ccc675475
Bence Király <kiralybencepecs@gmail.com>
2021-01-02 15:48:16 +0100

    Added file modification date to avoid caching issues

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,14 +10,14 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js') }}?lastmod={{ filemtime(public_path('js/app.js')) }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}?lastmod={{ filemtime(public_path('css/app.css')) }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
